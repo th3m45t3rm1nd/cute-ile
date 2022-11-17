2d0332abaf37d12c993528ca768e3f2e4b0d9bb5
Rahul Anand <rahul.anand1222004@gmail.com>
2022-11-17 10:54:29 +0530

    some changes in autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-feh --bg-scale /home/rahul/Downloads/wallhaven-eye8zw_1920x1080.png &
+feh --bg-scale /home/rahul/Documents/rahul\ files/Pics/For\ Ricing/wallhaven-eye8zw_1920x1080.png &
+
 picom &
